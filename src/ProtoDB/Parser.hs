3da1be7e2931b526cb72dfde4b094335029816f4
Travis Whitaker <twhitak@its.jnj.com>
2015-08-14 14:05:31 -0400

    Add documentation.

--- src/ProtoDB/Parser.hs
+++ src/ProtoDB/Parser.hs
@@ -9,6 +9,9 @@ Portability : POSIX
 
 This module provides parsers for generating ProtoDB records from other data
 sources.
+
+All parsers are expected to consume an entire "cell," i.e. must consume all of
+their input.
 -}
 
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass, LambdaCase #-}
@@ -39,12 +42,22 @@ import qualified Data.Attoparsec.ByteString.Lazy  as AL
 
 import qualified Data.ByteString.Lazy.Char8 as B
 
-emptyCell :: (Maybe a -> b) -> A.Parser b
+-- | Parse an empty cell as "missing data," i.e. 'Nothing' inside of a ProtoDB
+--   type constructor.
+emptyCell :: (Maybe a -> b) -- ^ External type constructor.
+          -> A.Parser b
 emptyCell c = A.endOfInput >> return (c Nothing)
 
+-- | Parse an integer cell. The empty string is recognized as "missing data,"
+--   otherwise the parser has the same behavior as Attoparsec's 'A.decimal'.
 parseProtoInt :: A.Parser ProtoInt
 parseProtoInt = emptyCell ProtoInt <|> (ProtoInt . Just) <$> (A.decimal <* A.endOfInput)
 
+-- | Parse a cell containing a real number. The empty string is recognized as
+--   "missing data," otherwise the parser has the same behavior as Attoparsec's
+--   'A.double', with the exption of allowing numbers between zero and one to be
+--   written without a leading zero digit. For example, 'A.double' would accept
+--   "0.123" but reject ".123" while this function accepts both.
 parseProtoReal :: A.Parser ProtoReal
 parseProtoReal = emptyCell ProtoReal <|> (ProtoReal . Just) <$> A.choice [noZero, A.double] <* A.endOfInput
     where noZero = do
@@ -53,6 +66,11 @@ parseProtoReal = emptyCell ProtoReal <|> (ProtoReal . Just) <$> A.choice [noZero
             let m = length (digitsRev 10 mts)
             return $ (fromIntegral mts) / (10 ^ m)
 
+-- | Parse a cell containing a string. The empty string is recognized as
+--   "missing data," otherwise the parser accepts all valid UTF8 strings. Note
+--   that arbitrary binary data may coincidentally constitute a valid UTF8
+--   string; make sure that the semantics of 'ProtoString' and 'ProtoBinary'
+--   are being appropriately applied.
 parseProtoString :: A.Parser ProtoString
 parseProtoString = emptyCell ProtoString <|> (ProtoString . Just) <$> (string <* A.endOfInput)
     where string = toUtf8 <$> A.takeLazyByteString >>=
@@ -63,9 +81,21 @@ parseProtoString = emptyCell ProtoString <|> (ProtoString . Just) <$> (string <*
 parseProtoDateTime :: A.Parser ProtoDateTime
 parseProtoDateTime = emptyCell ProtoDateTime <|> fail "Date/Time parser not implemented. Please help."
 
+-- | Parse a cell containing arbitrary binary data. The empty string is
+--   recognized as "missing data," otherwise the parser accepts any non-empty
+--   string.
 parseProtoBinary :: A.Parser ProtoBinary
 parseProtoBinary = emptyCell ProtoBinary <|> (ProtoBinary . Just) <$> (A.takeLazyByteString <* A.endOfInput)
 
+-- | This function parses a cell whose type is unknown. No type can be inferred
+--   from the empty string alone, so 'Nothing' is returned in this case. Parsers
+--   are tried in order, from least-accepting to most-accepting:
+--
+--   1. Integer
+--   2. Real
+--   3. Date/Time
+--   4. String (accepts any valid UTF8 string)
+--   5. Binary (accepts any binary data)
 parseProtoCell :: A.Parser (Maybe ProtoCell)
 parseProtoCell = (A.endOfInput >> return Nothing) <|> Just <$>
     A.choice [ (ProtoIntCell      <$> parseProtoInt)
@@ -104,10 +134,21 @@ finalGuess (CellTypeGuess i r s d b) = if (i == r) && (r == s) && (s == d) && (d
                , (ProtoBinaryType,   b)
                ]
 
+-- | "Guess" the type of a list of cells representing a single datablock record.
 firstRowHeuristic :: [B.ByteString] -> [Maybe ProtoCellType]
 firstRowHeuristic = map ((protoCellType <$>) . parseForType)
     where parseForType bs = (join . AL.maybeResult . AL.parse parseProtoCell) bs
 
+-- | "Guess" the field types of a sample of datablock records. This function is
+--   lazy; use it for small or streaming data sets.
+tallyRowHeuristic :: [[B.ByteString]] -> [Maybe ProtoCellType]
+tallyRowHeuristic (r:rs) = let l   = length r
+                               ins = zipWith tallyGuess . firstRowHeuristic
+                               e   = replicate l initCellTypeGuess
+                           in map finalGuess $ foldr ins e (r:rs)
+
+-- | Strict implementation of 'tallyRowHeuristic'. This function is strict; use
+--   it for large data sets.
 tallyRowHeuristic' :: [[B.ByteString]] -> [Maybe ProtoCellType]
 tallyRowHeuristic' (r:rs) = let l              = length r
                                 ins ts bs      = (seqList ts) `seq` zipWith tallyGuess (firstRowHeuristic bs) ts
@@ -116,8 +157,17 @@ tallyRowHeuristic' (r:rs) = let l              = length r
                                 seqList (x:xs) = x `seq` seqList xs
                            in map finalGuess $ foldl' ins e (r:rs)
 
-tallyRowHeuristic :: [[B.ByteString]] -> [Maybe ProtoCellType]
-tallyRowHeuristic (r:rs) = let l   = length r
-                               ins = zipWith tallyGuess . firstRowHeuristic
-                               e   = replicate l initCellTypeGuess
-                           in map finalGuess $ foldr ins e (r:rs)
+--tallyRowHeuristicPar :: Int -> [[B.ByteString]] -> [Maybe ProtoCellType]
+--tallyRowHeuristicPar c (r:rs) = let l              = length r
+--                                    ins ts cs      = (seqList ts) `seq` zipWith tallyGuess cs ts
+--                                    e              = replicate l initCellTypeGuess
+--                                    seqList []     = []
+--                                    seqList (x:xs) = x `seq` seqList xs
+--                           in map finalGuess $ foldl' ins e $ (map firstRowHeuristic (r:rs) `using` parBuffer c (evalList rdeepseq))
+
+--lenParseAnyCellPutM :: ProtoCell -> PutBlob
+--lenParseAnyCellPutM (ProtoInt      i) = lenMessageUnsafePutM i
+--lenParseAnyCellPutM (ProtoReal     r) = lenMessageUnsafePutM r
+--lenParseAnyCellPutM (ProtoString   s) = lenMessageUnsafePutM s
+--lenParseAnyCellPutM (ProtoDateTime d) = lenMessageUnsafePutM d
+--lenParseAnyCellPutM (ProtoBinary   b) = lenMessageUnsafePutM b
--- src/ProtoDB/Serializer.hs
+++ src/ProtoDB/Serializer.hs
@@ -16,48 +16,9 @@ functions for serialization, principally intended for converting CSV files.
 
 module ProtoDB.Serializer where
 
-import Control.Applicative
-
-import Control.Parallel.Strategies
-
-import Data.Int
-
-import Data.List
-
 import Data.ProtoBlob
 
 import qualified Data.ByteString.Lazy.Char8 as B
 
 import ProtoDB.Types
 import ProtoDB.Parser
-
-import qualified Data.Attoparsec.ByteString       as A
-import qualified Data.Attoparsec.ByteString.Char8 as A
-import qualified Data.Attoparsec.ByteString.Lazy  as AL
-
-data Field = Field {
-    fieldTitle  :: B.ByteString
-  , fieldType   :: ProtoType
-  , vectorShape :: [Int]
-  } deriving (Eq, Show)
-
-data DataBlock = DataBlock {
-    pdbTitle  :: B.ByteString
-  , pdbFields :: [Field]
-  , payload   :: [[B.ByteString]]
-  } deriving (Eq, Show)
-
---tallyRowHeuristicPar :: Int -> [[B.ByteString]] -> [Maybe ProtoCellType]
---tallyRowHeuristicPar c (r:rs) = let l              = length r
---                                    ins ts cs      = (seqList ts) `seq` zipWith tallyGuess cs ts
---                                    e              = replicate l initCellTypeGuess
---                                    seqList []     = []
---                                    seqList (x:xs) = x `seq` seqList xs
---                           in map finalGuess $ foldl' ins e $ (map firstRowHeuristic (r:rs) `using` parBuffer c (evalList rdeepseq))
-
---lenParseAnyCellPutM :: ProtoCell -> PutBlob
---lenParseAnyCellPutM (ProtoInt      i) = lenMessageUnsafePutM i
---lenParseAnyCellPutM (ProtoReal     r) = lenMessageUnsafePutM r
---lenParseAnyCellPutM (ProtoString   s) = lenMessageUnsafePutM s
---lenParseAnyCellPutM (ProtoDateTime d) = lenMessageUnsafePutM d
---lenParseAnyCellPutM (ProtoBinary   b) = lenMessageUnsafePutM b
