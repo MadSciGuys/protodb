75bc199b0ab1f07fb0e245f1c204efb2465f2dd2
ilongsho <ilongsho@ITS.JNJ.com>
2015-09-08 10:44:02 -0400

    I've added some array related stuff to ProtoDB.Serializer.
    This is mainly for use with the EDW DBG, but hopefully it's general enough
    to warrant going in ProtoDB.

--- src/ProtoDB/Serializer.hs
+++ src/ProtoDB/Serializer.hs
@@ -11,14 +11,94 @@ This module provides a comprehensive type for representing a data block and
 functions for serialization, principally intended for converting CSV files.
 -}
 
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module ProtoDB.Serializer where
 
-import Data.ProtoBlob
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.Sequence as Q
 
-import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Array.IArray
+import Data.Ix
+import Text.ProtocolBuffers.Basic (Utf8(..))
 
-import ProtoDB.Types
-import ProtoDB.Parser
+import Data.ProtoBlob (PutBlob, lenMessagePutM)
+import Data.Word (Word32)
+
+import ProtoDB.Types (ProtoCell(..), ProtoCellType)
+import ProtoDB.Types.ProtoField (ProtoField(..)) 
+import ProtoDB.Types.ProtoType (ProtoType(..))
+import ProtoDB.Types.ProtoInt (ProtoInt(..))
+import ProtoDB.Types.ProtoReal (ProtoReal(..))
+import ProtoDB.Types.ProtoString (ProtoString(..))
+import ProtoDB.Types.ProtoBinary (ProtoBinary(..))
+
+fI :: (Integral n, Num x) => n -> x
+fI = fromIntegral
+
+putCollection :: (Foldable f)
+  => BLC.ByteString -> ProtoType -> f ProtoCell -> PutBlob
+putCollection title pcType
+  = putArray title pcType ((:[]) . fromIntegral . length)
+
+putArray :: (Foldable f)
+  => BLC.ByteString 
+  -> ProtoType 
+  -> (f ProtoCell -> [Word32])
+  -> f ProtoCell 
+  -> PutBlob
+putArray title pcType toVecShape cells = do
+  lenMessagePutM 
+    $ ProtoField (Utf8 title) pcType (Q.fromList $ toVecShape cells)
+  mapM_ putCell cells
+
+tare1 :: (Ix i, Integral n) => Array i a -> [n]
+tare1 = (\ ((xl), (xr)) -> [fI $ rangeSize (xl, xr)] ) . bounds
+
+tare2 :: (Ix i, Integral n) => Array (i, i) a -> [n]
+tare2 = (\ ((xl, yl), (xr, yr)) 
+    -> map (fI . rangeSize) [(xl, xr), (yl, yr)]
+  ) . bounds
+
+tare3 :: (Ix i, Integral n) => Array (i, i, i) a -> [n]
+tare3 = (\ ((xl, yl, zl), (xr, yr, zr))
+    -> map (fI . rangeSize) [(xl, xr), (yl, yr), (zl, zr)]
+  ) . bounds
+
+tare4 :: (Ix i, Integral n) => Array (i, i, i, i) a -> [n]
+tare4 = (\ ((wl, xl, yl, zl), (wr, xr, yr, zr))
+    -> map (fI . rangeSize) [(wl, wr), (xl, xr), (yl, yr), (zl, zr)]
+  ) . bounds
+
+putCell :: ProtoCell -> PutBlob
+putCell = \case
+  ProtoIntCell      v -> p v
+  ProtoRealCell     v -> p v
+  ProtoStringCell   v -> p v
+  ProtoDateTimeCell v -> p v
+  ProtoBinaryCell   v -> p v
+  where p = lenMessagePutM
+
+{-
+class ProtoPut a where
+  toBlob :: a -> PutBlob
+
+instance ProtoPut ProtoCell where
+  toBlob = lenMessagePutM
+
+newtype PutInt n = PutInt n
+instance (Integral n) => ProtoPut (PutInt n) where
+  toBlob (PutInt n) = lenMessagePutM $ ProtoInt $ Just $ fromIntegral n
+
+newtype PutFloat x = PutFloat x
+instance (Real x) => ProtoPut (PutFloat x) where
+  toBlob (PutFloat x) = lenMessagePutM $ ProtoReal $ Just $ realToFrac x
+
+instance ProtoPut Utf8 where
+  toBlob = lenMessagePutM . ProtoString . Just
+
+instance ProtoPut BL.ByteString where
+  toBlob = lenMessagePutM . ProtoBinary . Just
+-}
